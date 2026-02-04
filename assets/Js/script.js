87b8db7c3ee25781bf96f5b16fe67f03c0c67688
DLStos12 <derick.stos@gmail.com>
2026-02-04 13:32:49 -0300

    Adição de loader

---
+++ assets/Js/script.js
@@ -0,0 +1,7 @@
+// Loader Script
+window.addEventListener('load', function() {
+    var loader = document.getElementById('loader');
+    var conteudo = document.getElementById('conteudo');
+    loader.style.display = 'none';
+    conteudo.style.display = 'block';
+});
